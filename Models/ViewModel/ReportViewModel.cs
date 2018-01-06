046ece4f6b2236cda11748d4d523d43832d47603
PATEL PC <patelpc.mits@gmail.com>
2018-01-06 21:05:40 +0530

    Added ReportViewModel

---
+++ Models/ViewModel/ReportViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace GoodLuck.Models.ViewModel
+{
+    public class ReportViewModel
+    {
+        public int Customer { get; set; }
+        public string ChallanNo { get; set; }
+        public string Size { get; set; }
+        public string Schedule { get; set; }
+        public DateTime ChallanFromDate { get; set; }
+        public DateTime ChallanToDate { get; set; }
+        public SelectList Customers { get; set; }
+        public List<Challan> Challans { get; set; }
+    }
+}
